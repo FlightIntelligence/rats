c2aa53f4c65ed42517b9f39675ed6f4488905f08
Hoang Tung Dinh <tunghdinh@gmail.com>
2016-09-27 17:30:00 +0200

    fixed some issues in boostrapping script

--- bootstrap/run_with_arlocros.py
+++ bootstrap/run_with_arlocros.py
@@ -22,22 +22,23 @@ def start():
 
 def start_single_master(processes, port, sync_config, bebop_ip, local_drone_ip, marker_config_file,
         use_threshold):
-    os.environ['ROS_MASTER_URI'] = 'http://localhost:' + port
-    os.environ['LOCAL_DRONE_IP'] = local_drone_ip
-    processes.append(subprocess.Popen(['roscore', '-p', port]))
+    my_env = os.environ.copy()
+    my_env['ROS_MASTER_URI'] = 'http://localhost:' + port
+    my_env['LOCAL_DRONE_IP'] = local_drone_ip
+    processes.append(subprocess.Popen(['roscore', '-p', port], env=my_env))
     time.sleep(2)
     master_discovery_cmd = 'rosrun master_discovery_fkie master_discovery _mcast_group:=224.0.0.1'
-    processes.append(subprocess.Popen(master_discovery_cmd.split()))
+    processes.append(subprocess.Popen(master_discovery_cmd.split(), env=my_env))
     time.sleep(2)
-    sync_cmd = 'rosrun master_sync_fkie master_sync _interface_url:="' + sync_config + '"'
-    processes.append(subprocess.Popen(sync_cmd.split()))
+    sync_cmd = 'rosrun master_sync_fkie master_sync _interface_url:=' + sync_config
+    processes.append(subprocess.Popen(sync_cmd.split(), env=my_env))
     time.sleep(2)
-    bebop_launch_cmd = 'roslaunch bebop_driver bebop_node.launch ip:="' + bebop_ip + '"'
-    processes.append(subprocess.Popen(bebop_launch_cmd.split()))
+    bebop_launch_cmd = 'roslaunch bebop_driver bebop_node.launch ip:=' + bebop_ip
+    processes.append(subprocess.Popen(bebop_launch_cmd.split(), env=my_env))
     time.sleep(2)
-    arlocros_launch_cmd = 'roslaunch rats bebop.launch marker_config_file:="' + \
-                          marker_config_file + '" use_threshold:="' + use_threshold + '"'
-    processes.append(subprocess.Popen(arlocros_launch_cmd.split()))
+    arlocros_launch_cmd = 'roslaunch rats bebop.launch marker_config_file:=' + \
+                          marker_config_file + ' use_threshold:=' + use_threshold
+    processes.append(subprocess.Popen(arlocros_launch_cmd.split(), env=my_env))
 
 
 def clean_up(processes):
