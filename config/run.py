42771adbd4b8c233d5764c1985f4550b3c899449
Hoang Tung Dinh <tunghdinh@gmail.com>
2016-09-29 10:53:44 +0200

    refactored run.py

--- config/run.py
+++ config/run.py
@@ -10,35 +10,64 @@ def start():
     processes = []
     print('Start the program...')
 
-    start_single_master(processes, '11311', sync_config, '192.168.32.1', '192.168.32.23',
-        'markerPingPongExtended.cfg', 'false')
+    start_single_master(
+        process_list=processes,
+        port='11311',
+        sync_config=sync_config,
+        bebop_ip='192.168.32.1',
+        local_drone_ip='192.168.32.23',
+        marker_config_file='markerPingPongExtended.cfg',
+        use_threshold='false')
 
-    start_single_master(processes, '11312', sync_config, '192.168.42.1', '192.168.42.87',
-        'markerPingPongExtended.cfg', 'false')
+    start_single_master(
+        process_list=processes,
+        port='11312',
+        sync_config=sync_config,
+        bebop_ip='192.168.42.1',
+        local_drone_ip='192.168.42.87',
+        marker_config_file='markerPingPongExtended.cfg',
+        use_threshold='false')
 
     atexit.register(clean_up, processes)
     input()
 
 
-def start_single_master(processes, port, sync_config, bebop_ip, local_drone_ip, marker_config_file,
-        use_threshold):
+def start_single_master(process_list, port, sync_config, bebop_ip, local_drone_ip,
+                        marker_config_file, use_threshold):
+    my_env = create_env(local_drone_ip, port)
+    launch_ros_master(my_env, port, process_list, sync_config)
+    launch_bebop_autonomy(bebop_ip, my_env, process_list)
+    launch_arlocros(marker_config_file, my_env, process_list, use_threshold)
+
+
+def launch_arlocros(marker_config_file, my_env, process_list, use_threshold):
+    arlocros_launch_cmd = 'roslaunch rats bebop.launch marker_config_file:=' + marker_config_file\
+                          + ' use_threshold:=' + use_threshold
+    process_list.append(subprocess.Popen(arlocros_launch_cmd.split(), env=my_env))
+
+
+def launch_bebop_autonomy(bebop_ip, my_env, process_list):
+    bebop_launch_cmd = 'roslaunch bebop_driver bebop_node.launch ip:=' + bebop_ip
+    process_list.append(subprocess.Popen(bebop_launch_cmd.split(), env=my_env))
+    time.sleep(2)
+
+
+def launch_ros_master(my_env, port, process_list, sync_config):
+    process_list.append(subprocess.Popen(['roscore', '-p', port], env=my_env))
+    time.sleep(2)
+    master_discovery_cmd = 'rosrun master_discovery_fkie master_discovery _mcast_group:=224.0.0.1'
+    process_list.append(subprocess.Popen(master_discovery_cmd.split(), env=my_env))
+    time.sleep(2)
+    sync_cmd = 'rosrun master_sync_fkie master_sync _interface_url:=' + sync_config
+    process_list.append(subprocess.Popen(sync_cmd.split(), env=my_env))
+    time.sleep(2)
+
+
+def create_env(local_drone_ip, port):
     my_env = os.environ.copy()
     my_env['ROS_MASTER_URI'] = 'http://localhost:' + port
     my_env['LOCAL_DRONE_IP'] = local_drone_ip
-    processes.append(subprocess.Popen(['roscore', '-p', port], env=my_env))
-    time.sleep(2)
-    master_discovery_cmd = 'rosrun master_discovery_fkie master_discovery _mcast_group:=224.0.0.1'
-    processes.append(subprocess.Popen(master_discovery_cmd.split(), env=my_env))
-    time.sleep(2)
-    sync_cmd = 'rosrun master_sync_fkie master_sync _interface_url:=' + sync_config
-    processes.append(subprocess.Popen(sync_cmd.split(), env=my_env))
-    time.sleep(2)
-    bebop_launch_cmd = 'roslaunch bebop_driver bebop_node.launch ip:=' + bebop_ip
-    processes.append(subprocess.Popen(bebop_launch_cmd.split(), env=my_env))
-    time.sleep(2)
-    arlocros_launch_cmd = 'roslaunch rats bebop.launch marker_config_file:=' + \
-                          marker_config_file + ' use_threshold:=' + use_threshold
-    processes.append(subprocess.Popen(arlocros_launch_cmd.split(), env=my_env))
+    return my_env
 
 
 def clean_up(processes):
@@ -49,4 +78,3 @@ def clean_up(processes):
 
 if __name__ == '__main__':
     start()
-
