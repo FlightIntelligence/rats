da2996da7dcd76701bb29559d46039dee29f6c66
Hoang Tung Dinh <tunghdinh@gmail.com>
2017-01-09 23:54:25 +0100

    Fix argument problem

--- config/land_publisher.py
+++ config/land_publisher.py
@@ -18,7 +18,7 @@ class LandPublisher:
     def start(self, child_server_ips):
         self._status = LandPublisher.Status.RUNNING
         publisher_thread = threading.Thread(target=self._publish_land_message,
-                                            args=child_server_ips)
+                                            args=(child_server_ips,))
         publisher_thread.start()
 
     def stop(self):
