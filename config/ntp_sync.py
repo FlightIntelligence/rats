35fde8dfbc512be41331e8297ded3db4a3573db4
Hoang Tung Dinh <tunghdinh@gmail.com>
2017-01-09 19:51:42 +0100

    Add more print info to ntp_sync.py

--- config/ntp_sync.py
+++ config/ntp_sync.py
@@ -6,12 +6,16 @@ import time
 if __name__ == '__main__':
     host = sys.argv[1]
 
+    print("Start ntp loader")
     while True:
         response = os.system('ping -c 1 ' + host)
         if response == 0:
+            print("Ping successfully")
             break
+        print("Cannot ping " + str(host) + "yet.")
         time.sleep(1)
 
     subprocess.call('service ntp stop'.split())
     subprocess.call('ntpd -gq'.split())
     subprocess.call('service ntp start'.split())
+    print("Time synced!")
